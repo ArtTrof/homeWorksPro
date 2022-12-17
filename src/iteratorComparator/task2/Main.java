018588a1b7e48290d5620e1577e511430cfee6c3
Artem Trofymenko <randallsng@gmail.com>
2022-12-17 16:00:02 +0100

    commit msg

---
+++ src/iteratorComparator/task2/Main.java
@@ -0,0 +1,25 @@
+package iteratorComparator.task2;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+
+        al.add(1);
+        al.add(2);
+        al.add(3);
+        al.add(4);
+        al.add(5);
+        al.add(6);
+        al.add(7);
+        al.add(8);
+        al.add(9);
+        al.add(10);
+
+        Iterator<Integer> iterator = al.iterator();
+
+    }
+}
---
+++ src/iteratorComparator/task3/Main.java
@@ -0,0 +1,6 @@
+package iteratorComparator.task3;
+class Main{
+    public static void main(String[] args) {
+
+    }
+}
