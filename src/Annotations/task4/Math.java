e7f8bdf2c7f348f09431497d03630511ced8d879
Artem Trofymenko <randallsng@gmail.com>
2022-12-30 16:23:13 +0100

    commit

--- src/Annotations/task4/Math.java
+++ src/Annotations/task4/Math.java
@@ -1,7 +1,7 @@
 package Annotations.task4;
 
 public class Math {
-    @addingTwoNumbers(num1 = 500, num2 = 546)
+    @addingTwoNumbers(num1 = 100, num2 = 200)
 
     public void sum(int num1, int num2) {
         System.out.println(num1 + num2);
